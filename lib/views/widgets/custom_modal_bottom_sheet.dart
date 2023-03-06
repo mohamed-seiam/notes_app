5d0e4c9ef44c552f8a8878f7e3f39e383349df40
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-03-07 00:18:18 +0200

    Creating TextField and it's Style

--- lib/views/widgets/custom_modal_bottom_sheet.dart
+++ lib/views/widgets/custom_modal_bottom_sheet.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'custom_text_field.dart';
+
 class CustomBottomSheet extends StatelessWidget {
   const CustomBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  Padding(
+      padding:const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
