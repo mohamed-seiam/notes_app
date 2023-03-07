dbe784c38b945534d195ef51fbcdb9f2b0c8a5ef
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-03-08 01:30:23 +0200

    Create Note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,10 @@
+class NoteModel
+{
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color ;
+
+  NoteModel(this.title, this.subTitle, this.date, this.color);
+
+}
